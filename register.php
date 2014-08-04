66e0086c6d4675a6687ff0614bd4d453eb3abd6e
root <root@li324-203.members.linode.com>
2014-08-04 05:11:54 +0000

    db stuff, html

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<title>Premium Coin Exchange</title>
+</head>
+<body>
+<div id="header">
+header
+</div>
+<div id="content">
+content
+</div>
+<div id="footer">
+footer
+</div>
+</body>
+</html>

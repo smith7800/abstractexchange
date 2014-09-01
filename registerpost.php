0f3f567be5554e170ed5d35dbcf0e15c4112cb3e
Smith <jeremydsmith@gmail.com>
2014-09-01 00:31:17 -0400

    user

--- registerpost.php
+++ registerpost.php
@@ -2,7 +2,7 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
-require_once("systemincludes.php");
-$tempInsert=userObj::insertUser( $dbObject,$first,$last,$email,$uname,$passw);
+require_once('systemincludes.php');
+$tempInsert=userObj::insertUser( &$dbObject,$userFirstName="yo",$userLastName="man",$userEmail="yo@man.om",$userUsername="yoman",$userPassword="yoandshit",$userUnverifiedFlag=1);
 
 ?>
--- systemincludes.php
+++ systemincludes.php
@@ -1,5 +1,6 @@
 <?php
 //include_once("globalfunctions.php");
+$dbConnect = new PDO('mysql:host=localhost;dbname=exchange;charset=utf8', 'root', 'Maddy.7800!!!!');
 require_once("classes/dbobj.php");
 require_once("classes/userobj.php");
 
